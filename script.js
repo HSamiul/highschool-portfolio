2eb33f12426af974ddccb1c848a227d270787af6
HSamiul <39920503+HSamiul@users.noreply.github.com>
2020-01-19 13:02:59 -0500

    changed dark theme color

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ document.body.onload = dark; // this function relies on the DOM being loaded, so
 function dark() {
 
     var myButton = document.createElement("button");
+
     myButton.innerHTML = "🌓"; //sets text of the button
     var body = document.getElementById("body"); //creates a  variable representing the body
-    body.appendChild(myButton); //adds the button to the body (so that it appears in the html)
+    body.appendChild(myButton); //adds the button to the body (so that it appears in the html)      
 
     body.classList.add("dark"); //it's like saying <body class = "dark">
     var site_element_list = document.getElementsByClassName("site_content"); //returns multiple elements that have the class; a list.
 
     myButton.addEventListener("click", function() { //activate toggle
         if (body.classList.contains("dark")) { //this if statement activates dark mode
-            document.getElementById("body").style.backgroundColor = "black"; //sets the body'd background to black
+            document.getElementById("body").style.backgroundColor = "#181a1b"; //sets the body'd background to black
 
             for (let step = 0; step < site_element_list.length; step++) { //set's site content (words) to white; iterates through site_element_list
                 site_element_list[step].style.color = "white";
@@ -22,7 +23,7 @@ function dark() {
         } else { //this else statement activates light mode. Basically inverting back the colors.
             document.getElementById("body").style.backgroundColor = "white";
             for (let step = 0; step < site_element_list.length; step++) {
-                site_element_list[step].style.color = "black";
+                site_element_list[step].style.color = "#181a1b";
             }
             body.classList.add("dark"); //adds back the dark class to body so that when we press the toggle again, dark mode is activated.
         }
